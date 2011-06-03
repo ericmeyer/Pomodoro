c7bc084d8d24119da644524b7b9faabd2cdf9dbb
Eric Meyer <emeyer@8thlight.com>
2011-06-03 15:49:36 -0500

    fixed whitespace and styling

--- Pomodoro/PomodoroViewController.m
+++ Pomodoro/PomodoroViewController.m
@@ -4,30 +4,25 @@
 
 @synthesize timerLabel, timer, pomo;
 
--(void)dealloc {
+-(void) dealloc {
     [super dealloc];
 }
 
--(void)didReceiveMemoryWarning {
+-(void) didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
-    
-    // Release any cached data, images, etc that aren't in use.
 }
-
-#pragma mark - View lifecycle
-
-// Implement viewDidLoad to do additional setup after loading the view, typically from a nib.
--(void)viewDidLoad {
+ 
+-(void) viewDidLoad {
     [super viewDidLoad];
     self.pomo = [[Pomodoro alloc] init];
 }
 
--(void)viewDidUnload {
+-(void) viewDidUnload {
     [super viewDidUnload];
     [self.pomo release];
 }
 
--(BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation {
+-(BOOL) shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation {
     return (interfaceOrientation == UIInterfaceOrientationPortrait);
 }
 
