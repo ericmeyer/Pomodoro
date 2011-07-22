5b9db35391c1378a743dad5d8e424636b0c4107e
Eric Meyer <emeyer@8thlight.com>
2011-07-22 15:11:33 -0500

    added a simple way to switch between normal and small times for timers

--- Pomodoro/PomodoroViewController.h
+++ Pomodoro/PomodoroViewController.h
@@ -2,9 +2,17 @@
 #import "TimerDelegate.h"
 #import "Timer.h"
 
-#define POMODORO_DURATION 25*60
-#define SNOOZE_DURATION 1*60
-#define BREAK_DURATION 5*60
+#define TESTING_TIMING 0
+
+#if TESTING_TIMING
+    #define POMODORO_DURATION 8
+    #define SNOOZE_DURATION 5
+    #define BREAK_DURATION 5
+#else
+    #define POMODORO_DURATION 25*60
+    #define SNOOZE_DURATION 1*60
+    #define BREAK_DURATION 5*60
+#endif
 
 @interface PomodoroViewController : UIViewController<TimerDelegate> {
     UILabel* gogo;
